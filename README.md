daa3ac903d23764b2224d1f8e06766bed844fc78
Cecilia Periquet <periwhat@gmail.com>
2022-12-23 11:33:33 +0100

    4.5 Exercisses 1,2,3 DATABASE

--- README.md
+++ README.md
@@ -30,20 +30,20 @@ Por favor marca a continuación los ejericios que hayais terminado:
 - [x] 4. Prueba el proyecto
 - [x] 5. Entiende el código del proyecto
 
-4.2 Express JS I:
+  4.2 Express JS I:
 
 - [x] 1. Pedir todas las películas
 - [x] 2. Filtrar por género
 - [x] 3. Ordenar por nombre
 
-4.3 Express JS II:
+  4.3 Express JS II:
 
 - [x] 1. Servidor de estáticos para React
 - [x] 2. Servidor de estáticos para las fotos
 - [x] 3. Peticiones POST con body params
 - [x] 4. Mantener logada a la usuaria
 
-4.4 Express JS III:
+  4.4 Express JS III:
 
 - [x] 1. Obtener el id de la película a renderizar
 - [x] 2. Obtener la película
@@ -51,16 +51,16 @@ Por favor marca a continuación los ejericios que hayais terminado:
 - [x] 4. Renderiza la película
 - [x] 5. Añade estilos
 
-4.5 Bases de datos I:
+  4.5 Bases de datos I:
 
-- [ ] 1. Crear la base de datos
-- [ ] 2. Configura la base de datos en Node JS
-- [ ] 3. Haz un SELECT para obtener todas películas
+- [x] 1. Crear la base de datos
+- [x] 2. Configura la base de datos en Node JS
+- [x] 3. Haz un SELECT para obtener todas películas
 - [ ] 4. Mejora tu SELECT
 - [ ] 5. SELECT para el motor de plantillas
 - [ ] 6. Crear la tabla de usuarias
 
-4.6 Bases de datos I:
+  4.6 Bases de datos I:
 
 - [ ] 1. Registro de nuevas usuarias en el front
 - [ ] 2. Registro de nuevas usuarias en el back
@@ -70,7 +70,7 @@ Por favor marca a continuación los ejericios que hayais terminado:
 - [ ] 6. Recupera los datos del perfil de la usuaria desde el front
 - [ ] 7. Recupera los datos del perfil de la usuaria desde el back
 
-4.7 Bases de datos I:
+  4.7 Bases de datos I:
 
 - [ ] 1. Pensar qué relación es
 - [ ] 2. Crear una relación N a N
--- src/index.js
+++ src/index.js
@@ -1,53 +1,78 @@
-const express = require("express");
-const cors = require("cors");
-const movies = require("./data/movies.json");
-const users = require("./data/users.json");
+const express = require('express');
+const cors = require('cors');
+/* const movies = require('./data/movies.json'); */
+const users = require('./data/users.json');
 
 // create and config server
 const server = express();
+const Database = require('better-sqlite3');
 server.use(cors());
 server.use(express.json());
 server.set('view engine', 'ejs');
+const db = Database('./src/data/movies.db', {
+  verbose: console.log,
+});
+
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get("/movies", (req, res) => {
-  const genderFilterParam = req.query.gender;
-  const filteredMovies = movies
-  .filter((movie) => {
+server.get('/movies', (req, res) => {
+  if (req.query.gender !== '') {
+    const query = db.prepare('SELECT * FROM movies WHERE gender = ?');
+    const movies = query.all(req.query.gender);
+    const response = {
+      success: true,
+      movies: movies,
+    };
+    res.json(response);
+  } else {
+    const query = db.prepare('SELECT * FROM movies');
+    const movies = query.all();
+    const response = {
+      success: true,
+      movies: movies,
+    };
+    res.json(response);
+  }
+});
+
+/* const genderFilterParam = req.query.gender;
+  const filteredMovies = movies.filter((movie) => {
     return movie.gender.toLowerCase().includes(genderFilterParam.toLowerCase());
   });
-    if (req.query.sort === "asc") {
-     filteredMovies.sort((a, b) => a.title.localeCompare(b.title));
-    } else {
+  if (req.query.sort === "asc") {
+    filteredMovies.sort((a, b) => a.title.localeCompare(b.title));
+  } else {
     filteredMovies.sort((a, b) => b.title.localeCompare(a.title));
-    }
+  }
 
   const response = {
     success: true,
     movies: filteredMovies,
   };
-  res.json(response);
-});
+  res.json(response); */
 
-server.post("/login", (req, res) => {
+server.post('/login', (req, res) => {
   const filteredUsers = users.find((user) => {
-    return user.email === req.body.userEmail && user.password === req.body.userPassword;
+    return (
+      user.email === req.body.userEmail &&
+      user.password === req.body.userPassword
+    );
   });
   console.log(filteredUsers);
   if (filteredUsers) {
-     const response = {
+    const response = {
       success: true,
-      userId: "id_de_la_usuaria_encontrada",
+      userId: 'id_de_la_usuaria_encontrada',
     };
     res.json(response);
   } else {
     const response = {
       success: false,
-      errorMessage: "Usuaria/o no encontrada/o",
+      errorMessage: 'Usuaria/o no encontrada/o',
     };
     res.json(response);
   }
@@ -56,15 +81,13 @@ server.post("/login", (req, res) => {
 server.get('/movie/:movieId', (req, res) => {
   const foundMovie = movies.find((movie) => movie.id === req.params.movieId);
   res.render('movie', foundMovie);
-  });
+});
 
-
-
-const staticServerPath = "./src/public-react";
+const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
 
-const staticServerPhotoPath = "./src/public-movies-images";
+const staticServerPhotoPath = './src/public-movies-images';
 server.use(express.static(staticServerPhotoPath));
 
-const staticServerStyles = "./src/public-movies-styles";
+const staticServerStyles = './src/public-movies-styles';
 server.use(express.static(staticServerStyles));
